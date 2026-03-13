d67f458a6e94647b43b8c13b6b7e003a6984fddb
TVaissiere <TVaissiere@outlook.com>
2026-03-13 16:29:04 +0000

    created main function - is running into error - needs fixing

--- backend/scripts/python/password_registration_checker.py
+++ backend/scripts/python/password_registration_checker.py
@@ -9,7 +9,19 @@ special_chars = ["!", "@", "#", "$", "%", "^", "&", "*"]
 keybrd_wlks = ["qwerty"]
 # NOTE: Make sure you use the relative path for leak files
 # TODO: Find better password list (this is too restrictive)
-leaks = ["../../../assets/pass_lists/10k-most-common.txt"]
+leaks = ["assets/pass_lists/10k-most-common.txt"]
+
+functions = [
+    lambda: length_check(password, errors, error_message),
+    lambda: special_char_check(password, errors, special_chars),
+    lambda: upper_case_check(password, errors, error_message),
+    lambda: lower_case_check(password, errors, error_message),
+    lambda: number_check(password, errors, error_message),
+    lambda: disallowed_words(password, username, errors, error_message),
+    lambda: keybrd_wlk_check(password, errors, error_message, keybrd_wlks),
+    lambda: leak_check(password, errors, error_message, leaks),
+]
+
 """ TODO: When you add a function, if it hits an error, 
 write the error in the array then call the array and specific array placement of the corresponding error """
 errors = ["Passwords must contain at least 12 characters.","Passwords cannot exceed 128 characters", "Password must contain at least one special character (e.g. ! @ # $ % ^ & *)", "Password must contain at least one upper case character", "Password must contain at least one lower case character", "Password must contain at least one number", "Your password cannot contain your username, email or the platform name", "Please avoid keyboard walking (i.e. qwerty, etc.)", "This password was found in a data leak, please try another."] 
@@ -18,13 +30,13 @@ errors = ["Passwords must contain at least 12 characters.","Passwords cannot exc
 """ NOTE: error message should be set to equal the relevant error from the errors array, 
 rather than printed so when this is integrated with frontend you can post the error_message bac """
 error_message = ""
+final_error_output = []
 
 # NOTE: Variables for testing. THESE WILL BE REPLACED WITH POST INPUTS FROM REG FORM
-password = ""
+password = "Sukcnskdncjsped209832!@"
 username = ""
 email = ""
 
-""" TODO: Test new logic to see if returning 2 values works"""
 def length_check(password, errors, error_message):
     if len(password) < 12:
         error_message = errors[0]
@@ -33,13 +45,13 @@ def length_check(password, errors, error_message):
         error_message = errors[1]
         return False, error_message
     else:
-        return True, error_message
+        return True, None
 
 # Checks wether or not the users password contains any array element from special_chars
 def special_char_check(password, errors, special_chars):
     for i in range(len(password)):
         if password[i] in special_chars:
-            return True, error_message
+            return True, None
     error_message = errors[2]
     return False, error_message
 
@@ -49,7 +61,7 @@ def upper_case_check(password, errors, error_message):
         error_message = errors[3]
         return False, error_message
     else:
-        return True, error_message
+        return True, None
 
 # Checks if the converted upper case version of the users password matches the original
 def lower_case_check(password, errors, error_message):
@@ -57,14 +69,14 @@ def lower_case_check(password, errors, error_message):
         error_message = errors[4]
         return False, error_message
     else:
-        return True, error_message
+        return True, None
 
 # Checks each character to see if it is a number and confirms wether there is at least one num present
 def number_check(password, errors, error_message):
     for i in range(len(password)):
         for x in range(9):
             if password[i] == str(x):
-                return True
+                return True, None
     error_message = errors[5]
     return False, error_message
 
@@ -81,15 +93,15 @@ def disallowed_words(password, username, errors, error_message):
         error_message = errors[6]
         return False, error_message
     else:
-        return True, error_message
-
+        return True, None
+    
 # Checks if the password contains specific keyboard walks
 def keybrd_wlk_check(password, errors, error_message, keybrd_wlks):
     for i in range(len(keybrd_wlks)):
         if keybrd_wlks[i] in password.lower():
             error_message = errors[7]
             return False, error_message
-    return True
+    return True, None
 
 # Checks if the users password has been recently leaked
 def leak_check(password, errors, error_message, leaks):
@@ -99,5 +111,17 @@ def leak_check(password, errors, error_message, leaks):
                 if x.strip("\n") in password.lower():
                     error_message = errors[8]
                     return False, error_message
-    return True, error_message
+    return True, None
 
+""" FIXME: Triggering 'Your password cannot contain your username, email 
+or the platform name' even when they are not the same"""
+def main(functions):
+    for function in functions:
+        result, error_message = function()
+        if result:
+            pass
+        else:
+            final_error_output.append(error_message)
+    print(final_error_output)
+        
+main(functions)
