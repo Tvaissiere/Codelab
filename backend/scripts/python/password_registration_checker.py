f0b570a399506ec44925004f4c9e902f1efd82ff
TVaissiere <TVaissiere@outlook.com>
2026-03-13 08:30:45 +0000

    slendered length check logic (removed length_check_result var)

--- backend/scripts/python/password_registration_checker.py
+++ backend/scripts/python/password_registration_checker.py
@@ -12,28 +12,22 @@ errors = ["Passwords must contain at least 12 characters.","Passwords cannot exc
 """ NOTE: error message should be set to equal the relevant error from the errors array, 
 rather than printed so when this is integrated with frontend you can post the error_message bac """
 error_message = ""
-length_check_result = ""
 
 # NOTE: Variables for testing. THESE WILL BE REPLACED WITH POST INPUTS FROM REG FORM
 password = ""
 username = ""
 email = ""
 
-""" FIXME: Make this more efficient. The logic works and it acheives the goal but it seems overly bulky. 
-Maybe remove length_check_result variable and just return True or False? """
-def length_check(password, length_check_result, errors, error_message):
+""" TODO: Test new logic to see if returning 2 values works"""
+def length_check(password, errors, error_message):
     if len(password) < 12:
-        length_check_result = False
         error_message = errors[0]
+        return False, error_message
     elif len(password) > 128:
-        length_check_result = False
         error_message = errors[1]
+        return False, error_message
     else:
-        length_check_result = True
-    if length_check_result == True:
-        return length_check_result
-    else:
-        return length_check_result, error_message
+        return True, error_message
 
 """ TODO: Make the function enumerate through each character in the password and check whether the char is in 
 special_char array. If no char is in special_char array then error_message = errors[2]? Unless you can think of
